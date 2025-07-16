42e3b08a29c902b16a6b3e53be9b5af5561481a6
Shadman Shadab <oneshadab@gmail.com>
2025-07-16 20:11:37 +0600

    chore: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opal-mcp-adapter",
-    version="0.0.4",
+    version="0.0.5",
     description=(
         "Bidirectional adapter for converting between MCP and Opal tools"
     ),
